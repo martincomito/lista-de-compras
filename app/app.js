a0cdc728d1d4fe30ef3edd1ff5453da1bb43d105
Martín Comito <martin@cohley.com>
2021-10-11 20:39:56 -0300

    Agregando configuración de Firebase

--- app/app.js
+++ app/app.js
@@ -1,7 +1,22 @@
 import Vue from 'nativescript-vue'
 
-import Home from './components/Home'
+import Home from './views/Home'
 Vue.config.silent = false
+
+// NativeScript 7+
+var firebase = require("@nativescript/firebase").firebase;
+
+firebase.init({
+  // Optionally pass in properties for database, authentication and cloud messaging,
+  // see their respective docs.
+}).then(
+    function () {
+      console.log("firebase.init done");
+    },
+    function (error) {
+      console.log("firebase.init error: " + error);
+    }
+);
 
 new Vue({
   render: (h) => h('frame', [h(Home)]),
